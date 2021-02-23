15af416d789b99dafc625952c3ddc57ce296f4dc
akash chhetri <akash.chhetri1@gmail.com>
2021-02-23 15:06:29 +1100

    new task added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,19 @@ class TasksController < ApplicationController
   def find 
     @task = Task.find(params["id"])
   end
+  def create
+    @task = Task.new(task_attributes)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  private
+  
+  def task_attributes
+    params.require(:task).permit(:title, :details, :completed)  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  get 'tasks/new', to: 'tasks#new', as: 'create_task_form'
+  post 'tasks', to: 'tasks#create', as: 'create_task'
+  
   get 'tasks/:id', to: 'tasks#find', as: 'task'
 end
